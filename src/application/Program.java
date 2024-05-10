9e77f5379ac5b467bbdd5d5594f3e43cb3cec9c5
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-10 15:37:02 -0300

    Segunda solução - Ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,39 +25,39 @@ public class Program {
 		Date checkIn = sdf.parse(sc.next());
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
-		
+
 		/*
 		 * Para testar se a data de checkIn não é posterior a data de CheckOut
 		 */
-		if(!checkOut.after(checkIn)) {
+		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}else {
-			Reservation reservation = new Reservation(number, checkIn,checkOut);
+		} else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-			
+
 			System.out.println();
 			System.out.println("Entrt data to update the reservation:");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-			
+
 			/*
-			 * Para a atualização, as datas não podem ser anteriores as da passada  
-			 */ 
-			Date now = new Date();
-			if(checkIn.before(now)||checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}else if(!checkOut.after(checkIn)){
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-			}else {
-				reservation.updateDates(checkIn, checkOut);
+			 * reservation.updateDates() vai retornar um string, então criou-se uma variável
+			 * "error" para receber essa string. Se "error" retornar null, não tem erro, mas
+			 * se ela retornar diferente de null, vai ser imprimido o tipo de erro
+			 */
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation : " + error);
+
+			} else {
+
 				System.out.println("Reservation: " + reservation);
 			}
-			
-			
+
 		}
-		
-		
+
 	}
 }
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -9,7 +9,7 @@ public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
@@ -37,30 +37,36 @@ public class Reservation {
 	public long duration() {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-		
+
 	}
-	
-	public void updateDates (Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
+		/*
+		 * Para a atualização, as datas não podem ser anteriores as da passada
+		 */
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Error in reservation: Check-out date must be after check-in date";
+		}
+		/*
+		 * Após verificar os dois possíveis erros, se passar e não indicar nenhum erro,
+		 * será atualizado as datas de CheckIn e CheckOut
+		 */
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		
+		/*
+		 * Se retornar null, não deu nenhum erro
+		 */
+		return null;
 	}
 
 	@Override
 	public String toString() {
-		return "Room " 
-				+ roomNumber 
-				+ ", checkIn: " 
-				+ sdf.format(checkIn)
-				+ ", checkOut: " 
-				+ sdf.format(checkOut) 
-				+ ", "
-				+ duration()
-				+ " nights";
+		return "Room " + roomNumber + ", checkIn: " + sdf.format(checkIn) + ", checkOut: " + sdf.format(checkOut) + ", "
+				+ duration() + " nights";
 	}
-	
-	
-	
-	
-	
+
 }
